f709bbe0d6baff1472b7a66089270c559d161710
richie-rich90454 <rjiang880@gmail.com>
2026-05-13 15:59:54 +0800

    feat(auth): make OAuth2 redirect URI configurable via REDIRECT_URI env var

--- server/api/login.get.ts
+++ server/api/login.get.ts
@@ -3,7 +3,7 @@ import { randomBytes } from "crypto"
 export default defineEventHandler(async (event) => {
     const state = randomBytes(128).toString("base64url")
     const origin = process.env.CURRENT_URL_ORIGIN || 'http://localhost:3000'
-    const redirectUri = encodeURIComponent(`${origin}/api/auth`)
+    const redirectUri = encodeURIComponent(`${origin}/${process.env.REDIRECT_URI}`||`${origin}/api/auth`)
     const url = `/api/oauth2/authorize?client_id=97e435f4-17e8-42ef-9b12-9684fd656de9&response_type=code&redirect_uri=${redirectUri}&scope=openid%20profile%20email&state=` + state
     await sendRedirect(event, url, 301)
 })
