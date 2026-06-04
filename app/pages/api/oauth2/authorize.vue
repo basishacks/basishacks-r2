adf3e26e25d11b7cc23d44036a52566987a5ac10
richie-rich90454 <rjiang880@gmail.com>
2026-06-04 18:08:05 +0800

    chore: relocate static app assets from public/assets/ to public/img/

--- app/pages/api/oauth2/authorize.vue
+++ app/pages/api/oauth2/authorize.vue
@@ -57,7 +57,7 @@
 
                       <UFormField name="email" label="or use the following...">
                         <UButton :disabled="isLoading" @click="navigateToOAuth2"> 
-                          <img src="/assets/Microsoft_logo.svg" alt="Microsoft Logo" class="w-5 h-5 mr-2" >
+                          <img src="/img/Microsoft_logo.svg" alt="Microsoft Logo" class="w-5 h-5 mr-2" >
                           Login with Microsoft
                         </UButton>
                       </UFormField>
