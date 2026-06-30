57aa982c6a0edfa10bd572ec083d21b8da21ba2c
richie-rich90454 <rjiang880@gmail.com>
2026-06-30 19:03:18 +0800

    drizzle: migrate plugin and types to use Drizzle ORM

--- server/types/cloudflare.d.ts
+++ server/types/cloudflare.d.ts
@@ -1,9 +1,11 @@
-import type { SQLiteDatabase } from '../utils/database'
+import type { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
+import type * as schema from '../database/schema'
 
 declare module 'h3' {
   interface H3EventContext {
     cf: CfProperties
-    db: SQLiteDatabase
+    /** Drizzle ORM instance */
+    drizzle: BetterSQLite3Database<typeof schema>
   }
 }
 
