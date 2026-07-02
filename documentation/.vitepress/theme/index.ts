8972809000e93055a7cdd983f692f07aea73fd92
richie-rich90454 <rjiang880@gmail.com>
2026-07-02 15:26:14 +0800

    feat(docs): register custom components and easter eggs
    Extend VitePress default theme with interactive components and global easter-egg listeners.

--- documentation/.vitepress/theme/index.ts
+++ documentation/.vitepress/theme/index.ts
@@ -1,4 +1,31 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
 import './style.css'
 
-export default DefaultTheme
+import TerminalWindow from './components/TerminalWindow.vue'
+import StatusBadge from './components/StatusBadge.vue'
+import CopyButton from './components/CopyButton.vue'
+import CollapsibleDetails from './components/CollapsibleDetails.vue'
+import AnimatedCounter from './components/AnimatedCounter.vue'
+import InteractiveHero from './components/InteractiveHero.vue'
+import QuoteCycler from './components/QuoteCycler.vue'
+import EasterEggOverlay from './components/EasterEggOverlay.vue'
+
+import { useEasterEggs } from './composables/useEasterEggs'
+
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('TerminalWindow', TerminalWindow)
+    app.component('StatusBadge', StatusBadge)
+    app.component('CopyButton', CopyButton)
+    app.component('CollapsibleDetails', CollapsibleDetails)
+    app.component('AnimatedCounter', AnimatedCounter)
+    app.component('InteractiveHero', InteractiveHero)
+    app.component('QuoteCycler', QuoteCycler)
+    app.component('EasterEggOverlay', EasterEggOverlay)
+  },
+  setup() {
+    useEasterEggs()
+  },
+} satisfies Theme
