6d5ea95132f2b62b8e9feea78c1a324b24f87a0f
Relizc <3464840574@qq.com>
2026-07-07 14:56:24 -0400

    merge

--- server/api/teams/index.get.ts
+++ server/api/teams/index.get.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
 
@@ -16,38 +15,11 @@ export default defineEventHandler(async (event) => {
         const seasonId = query.season_id ? Number(query.season_id) : -1;
         const teams =
             seasonId === -1 ? await getAllTeams(event) : await getTeamsBySeason(event, seasonId);
-=======
-import { DevPermissions } from "~~/shared/permissions";
-
-export default defineEventHandler(async (event) => {
-    //await requirePermission(event, DevPermissions.PORTAL_TEAMS_VIEW)
-    // u technically dont need this anymore ... since u can just GET /api/teams/1 without any perms
-
-    const query = getQuery(event);
-
-    if (query.judging) {
-        const { id: userID } = await requireJudge(event);
-
-        const teams = await getSubmittedUnjudgedTeams(event, userID);
->>>>>>> score-release-patch
         const awardsByTeam = await getAwardsForTeams(
             event,
             teams.map((t) => t.id),
         );
 
         return teams.map((t) => convertTeamToPublic(t, false, awardsByTeam[t.id] ?? []));
-<<<<<<< HEAD
-=======
-    } else {
-        const seasonId = query.season_id ? Number(query.season_id) : -1;
-        const teams =
-            seasonId === -1 ? await getAllTeams(event) : await getTeamsBySeason(event, seasonId);
-        const awardsByTeam = await getAwardsForTeams(
-            event,
-            teams.map((t) => t.id),
-        );
-
-        return teams.map((t) => convertTeamToPublic(t, false, awardsByTeam[t.id] ?? []));
->>>>>>> score-release-patch
     }
 });
