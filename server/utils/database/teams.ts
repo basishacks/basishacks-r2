2e3d4deab56a86900e70f5c311d83070e92e8406
richie-rich90454 <rjiang880@gmail.com>
2026-07-10 16:05:17 +0800

    Update prettier from 3.9.4 to 3.9.5 and ran format again

--- server/utils/database/teams.ts
+++ server/utils/database/teams.ts
@@ -23,8 +23,7 @@ export async function getTeam(
 ): Promise<Team | null> {
     if (allSeason) {
         const row = event.context.drizzle.select().from(teams).where(eq(teams.id, teamID)).get() as
-            | Team
-            | undefined;
+            Team | undefined;
         return row ?? null;
     }
     const activeSeason = await getActiveSeason(event);
@@ -85,8 +84,7 @@ export async function getSubmittedTeams(event: H3Event): Promise<Team[]> {
  */
 export async function getTeamById(event: H3Event, teamID: number): Promise<Team | null> {
     const row = event.context.drizzle.select().from(teams).where(eq(teams.id, teamID)).get() as
-        | Team
-        | undefined;
+        Team | undefined;
     return row ?? null;
 }
 
